d99ec0727cc89abe678336bcbfbf4e480adbcc91
Acamerus <111820648+Acamerus@users.noreply.github.com>
2025-02-20 14:23:23 -0500

    Initial

--- DiceClass/Program.cs
+++ DiceClass/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Dice Class UML
+
+// +Public: numRolls, Topside int
+// -Private: getValue, random, numSides
+// Fields
+// -numSides:int
+// -numRolls : Random 
+// -topSide : int
+// Propertys:
+// +Topside: int
+
+// Methods:
+//+Dice (numSide: int)
+//+Roll(): int
+
+// 2 doces rolled repeat 5 times 3/5 times highest result wins the game using array method
+
+public class Dice {
+    private int numSides;
+    private Random dice;
+
+    private int topSide;
+    private 
+}
